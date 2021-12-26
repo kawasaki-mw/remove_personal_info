caee4f2c8917d0f8a8fa151613d310b9b703cf64
kawasaki-mw <t-hosokawa@mw.kawasaki-m.ac.jp>
2021-12-26 12:43:42 +0900

    これで大丈夫かも

--- main.py
+++ main.py
@@ -2,23 +2,56 @@ import streamlit as st
 import os
 import base64
 import pikepdf
+import hashlib
+from docx import Document
 
 
 st.title('著者情報削除')
-uploaded_file = st.file_uploader('Wordファイルを選んでください…', type='pdf')
+uploaded_file = st.file_uploader('ファイル（docxもしくはpdf）をアップロートしてください…', type=['docx', 'pdf'])
 
 if uploaded_file is not None:
-    pdf = pikepdf.open(uploaded_file)
-    pdf.docinfo["/Author"] = ""
-    pdf.save('tmp.pdf')
 
-    with open("tmp.pdf", "rb") as pdf_file:
-        content = pdf_file.read()
-        encoded_string = base64.b64encode(content)
-        encoded_string = encoded_string.decode()
-        href = f'<a href="data:application/pdf;base64,{encoded_string}" download="result.pdf">download</a>'
-        #href = f'<a download="result.pdf" href="data:application/pdf;base64,{encoded_string}">download</a>'
-        st.markdown(f"ダウンロードする {href}", unsafe_allow_html=True)
+    try:
+        root, ext = os.path.splitext(uploaded_file.name)
 
-    os.remove('tmp.pdf')
+        if ext == '.docx':
+            doc = Document(uploaded_file)
 
+            core_properties = doc.core_properties
+            meta_fields= ["author", "category", "last_modified_by", "comments", "content_status", "identifier", "keywords", "language", "subject", "title", "version"]
+            for meta_field in meta_fields:
+                setattr(core_properties, meta_field, "")
+
+            download_filename = uploaded_file.name#'result.docx'
+            output_filename = hashlib.sha224(uploaded_file.name.encode()).hexdigest()#'tmp.docx'
+            doc.save(output_filename)
+            with open(output_filename, mode="rb") as f:
+                content = f.read()
+                encoded_string = base64.b64encode(content)
+                encoded_string = encoded_string.decode()
+                href = f'<a href="data:application/docx;base64,{encoded_string}" download="{download_filename}">download</a>'
+                st.markdown(f"ダウンロードする {href}", unsafe_allow_html=True)
+
+            os.remove(output_filename)
+
+
+        elif ext == '.pdf':
+            pdf = pikepdf.open(uploaded_file)
+            pdf.docinfo["/Title"] = ""
+            pdf.docinfo["/Author"] = ""
+            
+            download_filename = uploaded_file.name#'result.pdf'
+            output_filename = hashlib.sha224(uploaded_file.name.encode()).hexdigest()#'tmp.pdf'
+            pdf.save(output_filename)
+
+            with open(output_filename, "rb") as pdf_file:
+                content = pdf_file.read()
+                encoded_string = base64.b64encode(content)
+                encoded_string = encoded_string.decode()
+                href = f'<a href="data:application/pdf;base64,{encoded_string}" download="{download_filename}">download</a>'
+                st.markdown(f"ダウンロードする {href}", unsafe_allow_html=True)
+
+            os.remove(output_filename)
+
+    except Exception as e:
+        st.markdown(f"<b>エラーが発生しました: {str(e)}</b>", unsafe_allow_html=True)
